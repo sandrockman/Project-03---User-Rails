d72314761495871dbf26af2cd95d78529e304328
agentronin316 <svc-student@RC335G06.student.directory.jccc>
2015-09-19 12:33:49 -0500

    EditorGUILayout needs to be abandoned
    Got started on preparing the level for export.

---
+++ RailEngine/Assets/Editor/LevelExport.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.Collections.Generic;
+
+/// <summary>
+/// @author Marshall Mason
+/// </summary>
+public class LevelExport : EditorWindow
+{
+    [MenuItem("Rail Engine/Export Scene")]
+    public static void ExportScene()
+    {
+        Init();
+    }
+
+    static ScriptEngine engineScript;
+    List<string> outputText = new List<string>(0);
+    bool embedLevel = true;
+    int levelNumber;
+    string levelName;
+    string levelAuthor;
+
+    static void Init()
+    {
+        if (Selection.activeGameObject == null)
+        {
+            System.Media.SystemSounds.Exclamation.Play();
+            EditorUtility.DisplayDialog("No game object selected.",
+                "Please select a single game object with ScriptEngine and try again.", "Acknowledge");
+        }
+        else
+        {
+            engineScript = Selection.activeGameObject.GetComponent<ScriptEngine>();
+
+            if (engineScript == null)
+            {
+                System.Media.SystemSounds.Exclamation.Play();
+                EditorUtility.DisplayDialog("No ScriptEngine detected on selected object.",
+                    "Please select a single game object with ScriptEngine and try again.", "Acknowledge");
+            }
+            else
+            {
+                LevelExport window = (LevelExport)EditorWindow.GetWindow(typeof(LevelExport));
+                window.Show();
+            }
+        }
+    }
+
+    void OnGUI()
+    {
+        minSize = new Vector2(300, 300);
+        maxSize = minSize;
+
+        //EditorGUILayout on this looks like ass. Time to delve into EditorGUI
+        //EditorGUILayout.BeginHorizontal();
+        //EditorGUILayout.LabelField("Level Name");
+        //levelName = EditorGUILayout.TextField(levelName);
+        //EditorGUILayout.EndHorizontal();
+        //EditorGUILayout.BeginHorizontal();
+        //EditorGUILayout.LabelField("Level Author");
+        //levelAuthor = EditorGUILayout.TextField(levelAuthor);
+        
+    }
+}
